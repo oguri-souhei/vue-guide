f9c6a7f2463f56f96df971512d51520cc5c6e5db
souhei.oguri <souhei.oguri@gmail.com>
2023-07-22 15:25:25 +0900

    Vueアプリケーションの作成

---
+++ src/components/TodoDeleteButton.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const title = ref("TodoDeleteButton");
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,38 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+import TodoDeleteButton from "./components/TodoDeleteButton.vue";
+
+const app = createApp(App);
+
+/**************************
+ * アプリの設定
+ **************************/
+
+// アプリケーションのインスタンスは、.config オブジェクトを公開し、いくつかのアプリケーションレベルのオプションを設定することができます。
+// たとえば、すべての子孫コンポーネントから発生したエラーを捕捉するアプリケーションレベルのエラーハンドラーを定義するには次のとおりです
+app.config.errorHandler = (err) => {
+  console.error(err);
+};
+
+// アプリケーションのインスタンスは、アプリ用のアセットを登録するいくつかのメソッドもいくつか用意しています。
+// たとえば、次のようなコンポーネントの登録するメソッドがあります
+// このメソッドは、TodoDeleteButton をこのアプリケーション内でならどこでも使用できるようにしてくれます
+app.component("TodoDeleteButton", TodoDeleteButton);
+
+// アプリがマウントされる前に、アプリの設定がすべて適用されていることを確認しましょう！
+app.mount("#app");
+
+/**************************
+ * 複数アプリケーションもマウントできる
+ **************************/
+
+// const app1 = createApp({
+//   /* ... */
+// });
+// app1.mount("#container-1");
+
+// const app2 = createApp({
+//   /* ... */
+// });
+// app2.mount("#container-2");
